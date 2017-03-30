0736eb8ef0dcf25489683f7fba47dcd142aa74a4
Blake Hawkins <blake.hawkins@metaswitch.com>
2017-03-30 15:36:22 +0100

    use platform agnostic wrapper

--- bin/protoc-gen-scala.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set CLASSPATH="%SCALA_HOME%\lib\scala-library.jar"
-set SCRIPTPATH=%~dp0
-for %%i in ("%SCRIPTPATH%..\lib\*.jar") do set CLASSPATH=%CLASSPATH%;"%%i"
-for %%i in ("%SCRIPTPATH%..\target\scala-2.10\*.jar") do set CLASSPATH=%CLASSPATH%;"%%i"
-java -Xmx512M -cp %CLASSPATH% com.jeffplaisance.protobuf.compiler.ScalaProtoWrapperGenerator
