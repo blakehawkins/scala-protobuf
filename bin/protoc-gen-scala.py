d8570451260dcf5d4c5bb3c675cc9301b32cbb5c
Blake Hawkins <blake.hawkins@metaswitch.com>
2017-03-31 14:37:56 +0100

    improve python logging

--- bin/protoc-gen-scala.py
+++ bin/protoc-gen-scala.py
@@ -8,20 +8,29 @@ if __name__ == "__main__":
 
     java_class_name = 'com.jeffplaisance.protobuf.compiler.ScalaProtoWrapperGenerator'
 
-    SCRIPT     = subprocess.check_output(['readlink', '-f', sys.argv[0]])
-    SCRIPTPATH = subprocess.check_output(['dirname', SCRIPT])
+    SCRIPT     = subprocess.check_output(['readlink', '-f', sys.argv[0]]).decode('utf-8').strip()
+    SCRIPTPATH = subprocess.check_output(['dirname', SCRIPT]).decode('utf-8').strip()
 
     SCALA_HOME = os.environ['SCALA_HOME']
     CLASSPATH  = os.path.join(SCALA_HOME, 'lib', 'scala-library.jar')
 
-    for _, _, fileList in os.walk(os.path.join(SCRIPTPATH, b"..")):
+    walk_path = os.path.join(SCRIPTPATH, "..")
+    saved_paths = []
+    for dirpath, _, fileList in os.walk(walk_path):
         for fname in fileList:
+            check_path = os.path.join(dirpath, fname)
             if fname[-4:] == ".jar":
-                CLASSPATH = CLASSPATH + fname
+                saved_paths.append(check_path)
+                CLASSPATH = "{}:{}".format(CLASSPATH, check_path)
 
     with open('scala-protobuf-wrapper.log', 'w') as log:
-        log.write("{}\n".format(CLASSPATH))
-        log.write("{}\n".format(java_class_name))
+        log.write("saved_paths = {}\n".format(saved_paths))
+        log.write("walk_path   = {}\n".format(walk_path))
+        log.write("SCRIPT      = {}\n".format(SCRIPT))
+        log.write("SCRIPTPATH  = {}\n".format(SCRIPTPATH))
+        log.write("SCALA_HOME  = {}\n".format(SCALA_HOME))
+        log.write("CLASSPATH   = {}\n".format(CLASSPATH))
+        log.write("java_class_name = {}\n".format(java_class_name))
 
     subprocess.call(['java', '-Xmx512M', '-cp', CLASSPATH, java_class_name])
 
